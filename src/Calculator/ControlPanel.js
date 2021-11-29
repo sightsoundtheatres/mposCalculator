f2c0e895f191fa14244c61bc5b67180bad979547
alexsumoski <alex.sumoski@sight-sound.com>
2021-11-29 15:58:38 -0500

    added condition for smaller text, merged project

---
+++ src/Calculator/ControlPanel.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../styles/Calculator/_control-panel.scss';
+import History from '../assets/history.svg'
+
+const ControlPanel = (props) => (
+    <div className="control-panel">
+        {/* button for potential open close history */}
+        {/* <button className="open-history" onClick={props.onMenuToggle}>
+            <img src={History}/>
+        </button> */}
+    </div>
+);
+
+ControlPanel.defaultProps = {
+    anyHistory: false,
+    onToggleHistory: () => alert('toggle history')
+};
+
+ControlPanel.propTypes = {
+    anyHistory: PropTypes.bool,
+    onToggleHistory: PropTypes.func
+};
+
+export default ControlPanel;
