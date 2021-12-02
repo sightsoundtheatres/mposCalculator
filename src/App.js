ab9225066480dfedd7d834bca6211942f4cd22ae
alexsumoski <alex.sumoski@sight-sound.com>
2021-12-02 15:58:50 -0500

    remove

--- src/App.js
+++ src/App.js
@@ -3,15 +3,7 @@ import Calculator from './Calculator/Calculator';
 import './styles/app.scss';
 
 const App = () => (
-    <div>
-        {/* <img style={{width: '100%', minHeight: '100%', objectFit: 'cover', position: 'absolute', left: '0', top: '0'}} src="https://wallpaperaccess.com/full/31189.jpg"/>
-        <div style={{top: '50%', left: '50%', position: 'absolute', transform: 'translate(-50%,-50%)'}}>
-            <Calculator />
-        </div> */}
-
-        <Calculator />
-
-    </div>
+    <Calculator />
 );
 
 export default App;
